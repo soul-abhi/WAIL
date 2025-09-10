e7568e7e53ca12819df35b8e6dacb0250b6989b3
abhishek-kiet <abhishek.2428cse1292@kiet.edu>
2025-09-10 19:14:50 +0530

    Updated

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx,css}"],
-  theme: {
-    extend: {},
-  },
-  // this is for tailwind css configue please do not make any changes !! thank you 
-  plugins: [],
-}
